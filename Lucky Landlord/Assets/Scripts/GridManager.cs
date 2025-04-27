42f439c17f67b58c50b3ce45c4fdcca112801adc
Yousquid <ly2456@nyu.edu>
2025-04-27 11:08:49 +0800

    Value_fix

--- Lucky Landlord/Assets/Scripts/GridManager.cs
+++ Lucky Landlord/Assets/Scripts/GridManager.cs
@@ -685,7 +685,7 @@ public class GridManager : MonoBehaviour
                         gridObject.hasFogOfWar = false;
                         grid.TriggerGridObjectChanged(x, y);
                         revealedFogNumber++;
-                        fogRevealingPrice = 10 + (revealedFogNumber * 4)* (turnManager.currentTurn - 3) ;
+                        fogRevealingPrice = 10 + (revealedFogNumber * 4) ;
                         wealth -= fogRevealingPrice;
                         VisualizeGrid();
                     }
